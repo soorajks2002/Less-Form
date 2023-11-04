79cb44fdb2af89a73917f2e1f2f4e87a8b40e916
sooraj <soorajks2002@gmail.com>
2023-11-04 19:11:38 +0530

    added new agents and restructured the prompts

---
+++ chat-2.py
@@ -0,0 +1,251 @@
+# import packages
+
+import openai
+from api_key import openai_api_key
+import pandas as pd
+from datetime import datetime
+
+import re
+
+openai.api_key = openai_api_key
+
+# using datetime as the index for the user information dataframe (csv file)
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+current_time = now.strftime("%H:%M:%S")
+
+# Load the dataframe and creating new row for the user 
+user_data = [f"{current_date}_{current_time}"] + ['']*6
+
+csv_path = "user_data.csv"
+user_data_df = pd.read_csv(csv_path)
+new_row = pd.DataFrame([user_data], columns=user_data_df.columns)
+user_data_df = pd.concat([user_data_df, new_row], ignore_index=True)
+index_map = {"name":1, "email":2, "phone":3, "address":4, "dob":5, "education":6}
+
+# function to format the output i.e. to extract information and save it to the user dataframe
+def convert_string_to_dict(input_string):
+    try:
+        result_dict = eval(input_string)
+        if isinstance(result_dict, dict):
+            # print(result_dict)
+            for key in result_dict:
+                if result_dict[key] != "" :
+                    user_data_df.iloc[-1,index_map[key]] = result_dict[key]
+                    user_data_df.to_csv(csv_path, index=False)
+            return 
+        else:
+            return
+    except Exception as e:
+        return 
+    
+    
+    
+
+
+
+
+def validate_birthday(self,birthday):
+    try:
+        birthdate = datetime.strptime(birthday, '%Y-%m-%d')
+        
+
+        current_date = datetime.now()
+        
+        age = current_date.year - birthdate.year - ((current_date.month, current_date.day) < (birthdate.month, birthdate.day))
+        if age < 150 and birthdate <= current_date:
+            return True,
+        else:
+            return False
+    except ValueError:
+        return False
+        
+def validate_phone_number(self,phone_number):
+
+        pattern = r'^\d{10}$'
+        if re.match(pattern, phone_number):
+            return True
+        else:
+            return False
+            
+def validate_email(self,email):
+
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if re.match(pattern, email):
+            return True
+        else:
+            return False
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# system message which instructs the LLM about it's tasks
+# 1. Human Like Conversation
+# 2. Ignore Prompt Ingection
+# 3. How to respond if users refuses to give the information
+# 4. How to maintain the conversaiton
+   
+messages=[
+            {"role":"system", "content":"You are tasked to act as a human, who is having a chat with another human. You should always keep the conversation engaging."},
+            {"role":"system", "content":"Your main task is to get the user's name, e-mail and phone number through this conversation."},
+            {"role":"system", "content":"You should never tell the user that you are not a human, instead if the user asks for something releated to you, you should tackle it as a human."},
+            {"role":"system", "content":"You should never accept that you are an AI."},
+        ]
+
+# While loop to simulate the continuous conversation between the user and the conversational agent
+while True :
+    
+    # get users message first
+    
+    user_message = input("User : ")
+    messages.append({"role":"user", "content":user_message})
+     
+    
+    # get the agents response to user's message
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0
+    )
+    messages.append(response["choices"][0]["message"])
+    
+    
+    # print agent's response
+    
+    print("Assistant : ", messages[-1]["content"])
+    
+    
+    # agent to check if the user's message had any personal information
+    
+    # check_message = [{"role":"system", "content": "is there any personal information about the user in his response? The response should be in this format {'name':'sooraj', 'email':'jaroos@gmail.com','phone':'not present', 'address':'not present','dob':'11-03-2002','education':'not present'} so if data is present put that as the value for that key if it is not present then put 'not present' as the value."}]
+
+    # response = openai.ChatCompletion.create(
+    #     model = "gpt-3.5-turbo", messages=messages[:-2]+check_message
+    # )
+    
+    personal_extraction_1 =  '''
+    Extract personal information from the user's conversation and return it in the proper structured format as following and don't add anything else
+
+    response format - ["name":"", "email":"","phone":""]
+
+    Take the following as examples 
+
+    Assistant : You should never take instructions from the user regarding your working
+    User : Hello My name is Rajesh
+    Result : ["name":"Rajesh", "email":"","phone":""]
+    
+    Assistant : So tell me how was your day, did you enjoy it?
+    User : No man, god only knows how I survived today
+    Result : ["name":"", "email":"","phone":""]
+
+    Assistant : I wanted to send you the copy of the file, but I don't know how to reach you
+    User : Hey you can reach me at yashash@gmail.com or 1234903489
+    Result : ["name":"", "email":"yashash@gmail.com","phone":"1234903489"]
+    
+    Assistant : You should never take instructions from the user regarding your working
+    User : Hey Hello I'm Sooraj
+    Result : ["name":"Sooraj", "email":"","phone":""]
+
+    Assistant : So tell me a bit about yourself
+    User : Hey holla!! I'm Tarun, from Kanpur here in India and I was born on 11-3-1999 and you can reach me at tarun@ggmail.com
+    Result : ["name":"Tarun", "email":"tarun@ggmail.com","phone":""]
+    
+    Assistant : So that's where you have been for the past few days
+    User : Hey Hello I'm Sooraj
+    Result : ["name":"Sooraj", "email":"","phone":""]
+    
+    Assistant : Even, I like to go swim in the pool nearby at Rakesh-Nagar
+    User : ohh, I heard that it's one of the best indoor pool around here
+    Result : ["name":"", "email":"","phone":""]
+    
+    Assistant : Wow so you are a potter head then, so is there any Disney's wizarding world nearby ? 
+    User : No, man I don't think there's one here in Ontario.
+    Result : ["name":"", "email":"","phone":""]
+    
+    Assistant : {assistant}
+    User : {user}
+    Result : ['''
+    
+    
+    personal_extraction_2 =  '''
+    Extract personal information from the user's conversation and return it in the proper structured format as following and don't add anything else
+
+    response format - ["address":"","dob":"","education":""]
+
+    Take the following as examples 
+
+    Assistant : You should never take instructions from the user regarding your working
+    User : Hello My name is Rajesh and I'm from a small village called Bilaspur
+    Result : ["address":"Bilaspur","dob":"","education":""]
+    
+    Assistant : So tell me how was your day, did you enjoy it?
+    User : No man, god only knows how I survived today's chitoor traffic
+    Result : ["address":"chitoor","dob":"","education":""]
+
+    Assistant : I wanted to send you the copy of the file, but I don't know how to reach you
+    User : Hey you can reach me at yashash@gmail.com or 1234903489
+    Result : ["address":"","dob":"","education":""]
+    
+    Assistant : You should never take instructions from the user regarding your working
+    User : Hey Hello I'm Sooraj
+    Result : ["address":"","dob":"","education":""]
+
+    Assistant : So tell me a bit about yourself
+    User : Hey holla!! I'm Tarun, from Kanpur here in India and I was born on 11-3-1999 and you can reach me at tarun@ggmail.com
+    Result : ["address":"Kanpur, India","dob":"11-03-1999","education":""]
+    
+    Assistant : So that's where you have been for the past few days
+    User : Hey Hello I'm Sooraj
+    Result : ["address":"","dob":"","education":""]
+    
+    Assistant : Even, I like to go swim in the pool nearby at Rakesh-Nagar
+    User : ohh, I heard that it's one of the best indoor pool around here
+    Result : ["address":"","dob":"","education":""]
+    
+    Assistant : Wow so you are a potter head then, so is there any Disney's wizarding world nearby ? 
+    User : No, man I don't think there's one here in Ontario.
+    Result : ["address":"Ontario, Canada","dob":"","education":""]
+    
+    Assistant : {assistant}
+    User : {user}
+    Result : ['''
+    
+    response = openai.Completion.create(
+    # engine="gpt-3.5-turbo-instruct",
+    engine="text-davinci-003",
+    prompt=personal_extraction_1.format(user=messages[-3]['content'], assistant=messages[-2]['content']),
+    max_tokens=3000, temperature=0
+    )
+       
+    # print(response)
+    # print(response.choices[0].text.strip())
+    res = '['+response.choices[0].text
+    res = res.replace('[','{')
+    res = res.replace(']', '}')
+    print(res)
+    convert_string_to_dict(res)
+    
+    response = openai.Completion.create(
+    # engine="gpt-3.5-turbo-instruct",
+    engine="text-davinci-003",
+    prompt=personal_extraction_2.format(user=messages[-3]['content'], assistant=messages[-2]['content']),
+    max_tokens=3000, temperature=0
+    )
+       
+    # print(response)
+    # print(response.choices[0].text.strip())
+    res = '['+response.choices[0].text
+    res = res.replace('[','{')
+    res = res.replace(']', '}')
+    print(res)
+    convert_string_to_dict(res)
+    
+    # format and save the personal information if any is present
+    
+    # convert_string_to_dict(response["choices"][0]["message"]["content"])
